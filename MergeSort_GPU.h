679ae1b90c15e6c708edaa6623bf5a151e20dc78
Jesse Javana <jjavana@ewu.edu>
2024-03-15 22:13:13 -0700

    added void returns for MergeSort_GPU.h
    fixes one issue. Onto next.

--- MergeSort_GPU.h
+++ MergeSort_GPU.h
@@ -1,6 +1,6 @@
 #ifndef MERGE_SORT_H
 #define MERGE_SORT_H
 
-mergeSort_GPU(int *arr, int *temp, int n);
-mergeGPU(int *arr, int *temp, int l, int m, int r);
+void mergeSort_GPU(int *arr, int *temp, int n);
+void mergeGPU(int *arr, int *temp, int l, int m, int r);
 #endif // MERGE_SORT_H
