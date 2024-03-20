82e15ee75c3a077f60d4780454f899b0978895a1
Jesse Javana <jjavana@ewu.edu>
2024-03-19 22:36:34 -0700

    GPU almost works (array returns all zeros)

--- MergeSort_GPU.h
+++ MergeSort_GPU.h
@@ -1,7 +1,6 @@
 #ifndef MERGE_SORT_H
 #define MERGE_SORT_H
 
+void mergeSort_GPU(int *arr, int *temp, int n, int *sorted_arr);
 void mergeGPU(int *arr, int *temp, int l, int m, int r);
-__global__ void mergeSort_GPU(int *arr, int *temp, int n);
-
 #endif // MERGE_SORT_H
